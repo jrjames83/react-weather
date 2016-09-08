5898820d07fb4cafa10f5fe634db54796693e935
Jeffrey James <=>
2016-09-08 17:53:39 -0600

    ah

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,9 +2,10 @@ var React = require('react');
 
 var About = (props) => {
 	return (
+		<div>
 		<h3>About The Weather App</h3>
 		<p> Welcome to the about page</p>
-
+		</div>
 		)
 };
 
--- public/bundle.js
+++ public/bundle.js
@@ -26493,9 +26493,18 @@
 
 	var About = function About(props) {
 		return React.createElement(
-			'h3',
+			'div',
 			null,
-			'About Component'
+			React.createElement(
+				'h3',
+				null,
+				'About The Weather App'
+			),
+			React.createElement(
+				'p',
+				null,
+				' Welcome to the about page'
+			)
 		);
 	};
 
