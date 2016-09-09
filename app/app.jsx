9e3b7518266759c2ca6247bff4124676a4b792e8
Jeffrey James <=>
2016-09-08 18:21:35 -0600

    added foundation and styles

--- app/app.jsx
+++ app/app.jsx
@@ -7,6 +7,10 @@ var About = require('About');
 var Examples = require('Examples');
 
 
+// Load foundation
+require('style!css!foundation-sites/dist/foundation.min.css')
+$(document).foundation();
+
 /*
 
 http://api.openweathermap.org/data/2.5/weather
